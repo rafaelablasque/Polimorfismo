60e13b49b01a1292dbb14d21d04dda02ab45084d
rafaelablasque <rafaela.blasque@edu.senai.br>
2026-09-17 10:28:49 -0300

    exer3

---
+++ exer3/Animal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polimorfismo.exer3
+{
+    internal class Animal
+    {
+        public virtual void EmitirSom()
+        {
+            Console.WriteLine("Som de Animal")
+        }
+    }
+}
---
+++ exer3/Leao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polimorfismo.exer3
+{
+    internal class Leao : Animal
+    {
+        public override void EmitirSom()
+        {
+            Console.WriteLine("Leão: ROAR!");
+        }
+
+    }
+}
