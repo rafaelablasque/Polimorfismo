66daf749db7ab0066bb8a4f89e1521acf37894d2
rafaelablasque <rafaela.blasque@edu.senai.br>
2026-09-17 08:58:48 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Polimorfismo.exer1;
+
+namespace Polimorfismo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Geometria g = new Geometria();
+
+            double AreaQuadrado = g.CalcularArea(5);
+            double AreaRetangulo = g.CalcularArea(8, 20);
+            double AreaCirculo = g.CalcularArea(5, true);
+
+            Console.WriteLine("Area do Quadrado: " + AreaQuadrado);
+            Console.WriteLine("Area do Retangulo: " + AreaRetangulo);
+            Console.WriteLine("Area do Circulo: " + AreaCirculo);
+        }
+    }
+}
---
+++ exer1/Geometria.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polimorfismo.exer1
+{
+    internal class Geometria
+    {
+        public double CalcularArea(double Lado)
+        {
+            return Lado * Lado
+        }
+        public double CalcularArea(double Altura, double Base)
+        {
+            return Altura * Base;
+        }
+        public double CalcularArea(double Raio, bool IsCirculo)
+        {
+            return Math.PI * Raio * Raio;
+        }
+    }
+}
